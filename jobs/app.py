f682800122c0010b4c74b7829f7e8491dcbf132f
Ajna <ajna@192.168.0.109>
2020-02-08 22:55:39 +0100

    second steps

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH="db/jobs.sqlite"
 app = Flask(__name__)
 
 def open_connection():
-	connection = getattr(g, _connection, None)
+	connection = getattr(g, '_connection', None)
 	if connection == None:
 		connection = g._connection =sqlite2.connect(PATH)
 		connection.row_factory = sqlite.Row
@@ -24,7 +24,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-	connection = getattr(g, _connection, None)
+	connection = getattr(g, '_connection', None)
 	if connection != None:
 		connection.close()	
 
